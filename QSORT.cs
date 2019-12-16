16d6362d03a4e759c77af236711b297b55bfa0df
JSchutze <58947116+JSchutze@users.noreply.github.com>
2019-12-16 19:21:38 +0300

    Create QSORT.cs

---
+++ QSORT.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int left = 0;
+            int right = n - 1;
+            int[] array = new int[n];
+            for (int k = 0; k < n; k++)
+                Console.ReadLine();
+            QSort(array, left, right);
+            for (int k = 0; k < n; k++)
+                Console.WriteLine(array[k]);
+            Console.ReadKey();
+        }
+        static void QSort(int[] array, int left, int right)
+        {
+            int i = left;
+            int j = right;
+            int barrier = array[left];
+            while (i <= j)
+            {
+                while (array[i] < barrier)
+                {
+                    i++;
+                }
+                while (array[j] > barrier)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+            if (i < right)
+                QSort(array, i, right);
+            if (j > left)
+                QSort(array, left, j);
+
+        }
+
+
+
+    }
+}
